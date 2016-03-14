cca5902f3727a4a6f0a208c0e540724b0bc3c5d9
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-14 13:37:34 -0400

    Place cards, updated descriptions in README

--- README.md
+++ README.md
@@ -3,5 +3,21 @@
 ###Setup the table using Bootstrap. Create a dealers hand and players hand to hold the toals.
 Set each card to a custom width(adjusted col-sm-2 to 11% to avoid having to write 3 extra divs).
 Add 3 button to the bottom of the table so the user can interact with the game.
+
 ###Write some Javascript
+
 Bind each button to a jQuery click function and call the correct function depending on the click.
+
+- bind a function called deal to the deal button.
+
+-when called it will create a deck in default order, then swap 2 cards in the array 500 times.
+
+-once swaps are done, the deck will be shuffled. The number can be changed from 500 for varrying degress of randomness.
+
+- After the deck is shuffled, the Player Hand and Dealer Hand arrays are created with the 0,2 and 1,3 cards in theDeck array.
+
+- The Placecard function is then called which takes 3 paramenter: the playersHand array, who's turn it is, and the slot the card goes into.
+
+-placeCard then removes the "empty" class to accomadate styling and updates the HTML to use the card value.
+
+- calculateTotal fuction is then called which is sent two paramentes: the playerHand array and who's turn it is. It slices each card in the array via a loop and removes the letter on the end. The result is turned int o a Number to ensure we can run math functions on it and then the toal is updated with the new number.
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var theDeck = [];
+var placeInDeck = 0;
 
 $(document).ready(function(){
 
@@ -15,28 +16,63 @@ $(document).ready(function(){
 });
 
 function deal(){
-	theDeck = shuffleDeck();	
-
+	shuffleDeck();
+	playerHand = [theDeck[0], theDeck[2]];
+	dealerHand = [theDeck[1], theDeck[3]];
+	placeInDeck = 4;
+	placeCard(playerHand[0], "player", "one");
+	placeCard(dealerHand[0], "dealer", "one");
+	placeCard(playerHand[1], "player", "two");
+	placeCard(dealerHand[1], "dealer", "two");
+	calculateTotal(playerHand, "player");
+	calculateTotal(dealerHand, "dealer");		
 }
+
+function placeCard(card, who, slot){
+	var currID = "#" + who + "-card-" + slot;
+	$(currID).removeClass("empty");
+	$(currID).html(card);
+}
+function calculateTotal(hand, who){
+	var total = 0;
+	for(var i = 0; i<hand.length; i++){
+// purposely not fixing 11, 12, 13 or 1
+		var cardValue = Number(hand[i].slice(0,-1));
+		total += cardValue
+	}
+	var idToGet = "." + who + "-total";
+	$(idToGet).html(total);
+	//what if the total is over 21? this would be a good place to check for bust.
+}
+
 
 // s1 = hearts, s2=spades, s3=diamonds, s4=clubs
 function shuffleDeck(){
 	for(var s = 1; s <= 4; s++)	{
 		var suit = "";
 		if(s === 1){
-			suit = "h;"
+			suit = "h";
 		}else if(s===2){
-			suit = "s;"
+			suit = "s";
 		}else if(s===3){
-			suit = "d;"
+			suit = "d";
 		}else if(s===4){
-			suit = "c;"
+			suit = "c";
 		}
 		for(var i = 1; i <= 13; i++){
 			theDeck.push(i+suit);
 		}
 	}
-	console.log(theDeck);
+	var numberOFTimesToShuffle = 500;
+	for(var i = 1; i < numberOFTimesToShuffle; i++){
+		card1 = Math.floor(Math.random()*theDeck.length);
+		card2 = Math.floor(Math.random()*theDeck.length);
+		if(card1 != card2){
+			temp = theDeck[card1];
+			theDeck[card1] = theDeck[card2];
+			theDeck[card2] = temp;
+		}
+	}
 }
 
 
